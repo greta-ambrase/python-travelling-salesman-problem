670363bc815312f6c1c59778dfed5fb2f4790384
pop-one-demo <pop.one.demo.account@dcs.bbk.ac.uk>
2019-11-28 22:37:44 +0000

    Added functions for reading and printing the road map.

--- cities.py
+++ cities.py
@@ -1,3 +1,4 @@
+
 def read_cities(file_name):
     """
     Read in the cities from the given `file_name`, and return 
@@ -9,67 +10,86 @@ def read_cities(file_name):
 
       Alabama -> Alaska -> Arizona -> ... -> Wyoming -> Alabama.
     """
-    return True
+    file = open(file_name, 'r')
+    road_map = []
+    for i in file.readlines():
+        line = i.strip('\n')
+        tmp = line.split("\t")
+        road_map.append(tuple((tmp[0], tmp[1], float(tmp[2]), float(tmp[3]))))
+        
+    return road_map
+
   
 def print_cities(road_map):
     """
     Prints a list of cities, along with their locations. 
     Print only one or two digits after the decimal point.
     """
-    return True
+    for i in road_map:
+        for j in i:
+            if type(j) == float:
+                print(f'{j:15.2f}', end =', ')
+            else:
+                print(j, end = ', ')
+        print('\n')
 
-def compute_total_distance(road_map):
-    """
-    Returns, as a floating point number, the sum of the distances of all 
-    the connections in the `road_map`. Remember that it's a cycle, so that 
-    (for example) in the initial `road_map`, Wyoming connects to Alabama...
-    """
-    return True
 
-def swap_cities(road_map, index1, index2):
-    """
-    Take the city at location `index` in the `road_map`, and the 
-    city at location `index2`, swap their positions in the `road_map`, 
-    compute the new total distance, and return the tuple 
+# def compute_total_distance(road_map):
+#     """
+#     Returns, as a floating point number, the sum of the distances of all 
+#     the connections in the `road_map`. Remember that it's a cycle, so that 
+#     (for example) in the initial `road_map`, Wyoming connects to Alabama...
+#     """
+#     return True
 
-        (new_road_map, new_total_distance)
+# def swap_cities(road_map, index1, index2):
+#     """
+#     Take the city at location `index` in the `road_map`, and the 
+#     city at location `index2`, swap their positions in the `road_map`, 
+#     compute the new total distance, and return the tuple 
 
-    Allow for the possibility that `index1=index2`,
-    and handle this case correctly.
-    """
-    return True
+#         (new_road_map, new_total_distance)
 
-def shift_cities(road_map):
-    """
-    For every index i in the `road_map`, the city at the position i moves
-    to the position i+1. The city at the last position moves to the position
-    0. Return the new road map. 
-    """
-    return True
+#     Allow for the possibility that `index1=index2`,
+#     and handle this case correctly.
+#     """
+#     return True
 
-def find_best_cycle(road_map):
-    """
-    Using a combination of `swap_cities` and `shift_cities`, 
-    try `10000` swaps/shifts, and each time keep the best cycle found so far. 
-    After `10000` swaps/shifts, return the best cycle found so far.
-    Use randomly generated indices for swapping.
-    """
-    return True
+# def shift_cities(road_map):
+#     """
+#     For every index i in the `road_map`, the city at the position i moves
+#     to the position i+1. The city at the last position moves to the position
+#     0. Return the new road map. 
+#     """
+#     return True
 
-def print_map(road_map):
-    """
-    Prints, in an easily understandable format, the cities and 
-    their connections, along with the cost for each connection 
-    and the total cost.
-    """
-    return True
+# def find_best_cycle(road_map):
+#     """
+#     Using a combination of `swap_cities` and `shift_cities`, 
+#     try `10000` swaps/shifts, and each time keep the best cycle found so far. 
+#     After `10000` swaps/shifts, return the best cycle found so far.
+#     Use randomly generated indices for swapping.
+#     """
+#     return True
 
-def main():
-    """
-    Reads in, and prints out, the city data, then creates the "best"
-    cycle and prints it out.
-    """
-    return True
+# def print_map(road_map):
+#     """
+#     Prints, in an easily understandable format, the cities and 
+#     their connections, along with the cost for each connection 
+#     and the total cost.
+#     """
+#     return True
+
+# def main():
+#     """
+#     Reads in, and prints out, the city data, then creates the "best"
+#     cycle and prints it out.
+#     """
+#     return True
 
 if __name__ == "__main__": #keep this in
-    main()
+    road_map=read_cities('city-data.txt')
+    print_cities(road_map)
+
+#     main()
+
